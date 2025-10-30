20b972daef8c6b72bcfda1124dffc17b3040c954
Tymon Racinski <tymon.piotr@gmail.com>
2025-10-30 15:13:28 +0100

    feature11

--- src/account.py
+++ src/account.py
@@ -1,15 +1,18 @@
 class Account:
     def __init__(self):
         self.balance=0
+        self.history=[]
     
     def transaction(self, value, price=0):
         if self.balance+value>0:
             self.balance+=(value-price)
+            self.history.append({"value":value,"cost":price})
 
 class Personal_Account(Account):
     def __init__(self, first_name, last_name, pesel, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
+        self.history=[]
         
         self.balance = 0
         if promo_code == "PROMO_XYZ" and ( int(pesel[0:2])>60 or int(pesel[2])>1 ):
@@ -31,11 +34,9 @@ class Company_Account(Account):
         else: self.nip = "INVALID"
         self.company_name = company_name
         self.balance = 0
+        self.history=[]
     
     def transaction(self, value, express=False):
         price=0
         if express : price=5
         return super().transaction(value,price)
-    
-    def uncovered_method(self):
-        print("I'm off the hook")
